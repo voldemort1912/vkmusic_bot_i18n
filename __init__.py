42544114c97b2d9bd13b342c4bb58eba235d9252
Ilya Samartsev <ignz@yandex-team.ru>
2019-02-18 14:15:57 +0300

    add list of langs

---
+++ __init__.py
@@ -0,0 +1,17 @@
+LANG_PAIRS = [
+  ('en', u"English"),
+  ('ru', u"Русский"),
+  ('es', u"Español"),
+  ('it', u"Italiano"),
+  ('pt', u"Português"),
+  ('fa', u"فارسی"),
+  ('tr', u"Türkçe"),
+  ('uz', u"Oʻzbekcha"),
+  ('uk', u"Українська"),
+  ('de', u"Deutsch"),
+  ('fr', u"Français"),
+  ('nl', u"Nederlands"),
+  ('kz', u"Қазақ"),
+  ('id', u"Indonesian"),
+  ('ro', u"Română")
+]
